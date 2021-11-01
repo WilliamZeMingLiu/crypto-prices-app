26b0142665c4dc9cceaeed7e7f15d0a1135a300f
WilliamZeMingLiu <williamzemingliu@gmail.com>
2021-11-01 15:49:42 -0700

    changed base url

--- src/main/client/src/apis/priceApi.js
+++ src/main/client/src/apis/priceApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8080"
+const baseUrl = "https://eth-btc-crypto-prices-app.herokuapp.com"
 
 const getCoinbase = async (endpoint) => {
     const data = {
